5222551cc5a13e894c29c8247978a2e99898e51a
Eran Krakovsky <eran.krakovsky@appsflyer.com>
2022-02-16 10:37:48 +0200

    added RMSE to experiment

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.model_selection import ParameterSampler
 from datasets.movielens import get_movielens_dataset
 from evaluations.cross_validation import random_train_test_split
-from evaluations.evaluation import mrr_score
+from evaluations.evaluation import mrr_score, rmse_score
 from experiments.results import Results
 from factorization.implicit import ImplicitFactorizationModel
 from model_utils.layers import BloomEmbedding, ScaledEmbedding
@@ -17,7 +17,7 @@ NUM_SAMPLES = 20
 
 # sampling these hyperparameters for the baseline process
 LEARNING_RATES = [1e-4, 5 * 1e-4, 1e-3, 1e-2, 5 * 1e-2, 1e-1]
-LOSSES = ['bpr', 'adaptive_hinge']
+LOSSES = ["bpr", "adaptive_hinge"]
 BATCH_SIZE = [32, 64, 128, 256, 512]
 EMBEDDING_DIM = [32, 64, 128, 256]
 N_ITER = list(range(1, 20))
@@ -25,17 +25,17 @@ L2 = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 0.0]
 
 # hyperparameters for this experiment
 COMPRESSION_RATIOS = (np.arange(1, 10) / 10).tolist()
-HASH_FUNCTIONS = ['MurmurHash', 'xxHash', 'MD5', 'SHA1', 'SHA256']
+HASH_FUNCTIONS = ["MurmurHash", "xxHash", "MD5", "SHA1", "SHA256"]
 
 
 def set_hyperparameters(random_state, num):
     params = {
-        'n_iter': N_ITER,
-        'batch_size': BATCH_SIZE,
-        'l2': L2,
-        'learning_rate': LEARNING_RATES,
-        'loss': LOSSES,
-        'embedding_dim': EMBEDDING_DIM,
+        "n_iter": N_ITER,
+        "batch_size": BATCH_SIZE,
+        "l2": L2,
+        "learning_rate": LEARNING_RATES,
+        "loss": LOSSES,
+        "embedding_dim": EMBEDDING_DIM,
     }
     sampler = ParameterSampler(params, n_iter=num, random_state=random_state)
 
@@ -47,95 +47,163 @@ def build_factorization_model(hyperparameters, train, random_state):
     h = hyperparameters
     set_seed(42, CUDA)
 
-    if h['compression_ratio'] < 1.0:
-        item_embeddings = BloomEmbedding(train.num_items, h['embedding_dim'], compression_ratio=h['compression_ratio'], num_hash_functions=4, padding_idx=0, hash_function=hyperparameters['hash_function'])
-        user_embeddings = BloomEmbedding(train.num_users, h['embedding_dim'], compression_ratio=h['compression_ratio'], num_hash_functions=4, padding_idx=0, hash_function=hyperparameters['hash_function'])
+    if h["compression_ratio"] < 1.0:
+        item_embeddings = BloomEmbedding(
+            train.num_items,
+            h["embedding_dim"],
+            compression_ratio=h["compression_ratio"],
+            num_hash_functions=4,
+            padding_idx=0,
+            hash_function=hyperparameters["hash_function"],
+        )
+        user_embeddings = BloomEmbedding(
+            train.num_users,
+            h["embedding_dim"],
+            compression_ratio=h["compression_ratio"],
+            num_hash_functions=4,
+            padding_idx=0,
+            hash_function=hyperparameters["hash_function"],
+        )
     else:
-        item_embeddings = ScaledEmbedding(train.num_items, h['embedding_dim'], padding_idx=0)
-        user_embeddings = ScaledEmbedding(train.num_users, h['embedding_dim'], padding_idx=0)
+        item_embeddings = ScaledEmbedding(
+            train.num_items, h["embedding_dim"], padding_idx=0
+        )
+        user_embeddings = ScaledEmbedding(
+            train.num_users, h["embedding_dim"], padding_idx=0
+        )
 
-    network = Net(train.num_users, train.num_items, user_embedding_layer=user_embeddings, item_embedding_layer=item_embeddings)
-    model = ImplicitFactorizationModel(loss=h['loss'], n_iter=h['n_iter'], batch_size=h['batch_size'], learning_rate=h['learning_rate'], embedding_dim=h['embedding_dim'], l2=h['l2'], representation=network, use_cuda=CUDA, random_state=random_state)
+    network = Net(
+        train.num_users,
+        train.num_items,
+        user_embedding_layer=user_embeddings,
+        item_embedding_layer=item_embeddings,
+    )
+    model = ImplicitFactorizationModel(
+        loss=h["loss"],
+        n_iter=h["n_iter"],
+        batch_size=h["batch_size"],
+        learning_rate=h["learning_rate"],
+        embedding_dim=h["embedding_dim"],
+        l2=h["l2"],
+        representation=network,
+        use_cuda=CUDA,
+        random_state=random_state,
+    )
 
     return model
 
 
 def evaluate_model(model, train, test, validation):
-    start_time = time.time()
+    start_time = time.perf_counter()
     model.fit(train, verbose=True)
-    elapsed = time.time() - start_time
+    elapsed = time.perf_counter() - start_time
 
-    print(f'Elapsed {elapsed}')
-    print(model)
+    print(f"Elapsed {elapsed}")
 
     test_mrr = mrr_score(model, test)
     val_mrr = mrr_score(model, test.tocsr() + validation.tocsr())
-
-    return test_mrr, val_mrr, elapsed
+    test_rmse = rmse_score(model, test)
+    val_rmse = rmse_score(model, validation)
+    return test_mrr, val_mrr, test_rmse, val_rmse, elapsed
 
 
 def run(experiment_name, hash_function, train, test, validation, random_state):
+    print(f"Running experiment {experiment_name}....")
     results = Results(experiment_name)
     best_result = results.best()
 
     if best_result is not None:
-        print(f'Best result: {best_result}')
+        print(f"Best result: {best_result}")
 
     # Find a good baseline
     for hyperparameters in set_hyperparameters(random_state, NUM_SAMPLES):
-        hyperparameters['batch_size'] = hyperparameters['batch_size'] * 4
-        hyperparameters['compression_ratio'] = 1.0
-        hyperparameters['hash_function'] = ''
+        hyperparameters["batch_size"] = hyperparameters["batch_size"] * 4
+        hyperparameters["compression_ratio"] = 1.0
+        hyperparameters["hash_function"] = ""
 
         if hyperparameters in results:
-            print('Done, skipping...')
+            print("Done, skipping...")
             continue
 
-        print(f'==> Hyperparameters: {hyperparameters}')
+        print(f"==> Hyperparameters: {hyperparameters}")
         model = build_factorization_model(hyperparameters, train, random_state)
-        test_mrr, val_mrr, elapsed = evaluate_model(model, train, test, validation)
+        test_mrr, val_mrr, test_rmse, val_rmse, elapsed = evaluate_model(
+            model, train, test, validation
+        )
 
-        print(f'==> Test MRR: {test_mrr.mean()}, Validation MRR: {val_mrr.mean()}, Elapsed Time: {elapsed}')
-        results.save(hyperparameters, test_mrr.mean(), val_mrr.mean(), elapsed)
+        print(
+            f"==> Test MRR: {test_mrr.mean()}, \
+                Validation MRR: {val_mrr.mean()}, \
+                Test RMSE: {test_rmse}, \
+                Validation RMSE: {val_rmse},\
+                Elapsed Time: {elapsed}"
+        )
+        results.save(
+            hyperparameters,
+            test_mrr.mean(),
+            val_mrr.mean(),
+            test_rmse,
+            val_rmse,
+            elapsed,
+        )
 
     best_baseline = results.best_baseline()
-    print(f'Best baseline: {best_baseline}')
-    print('*'*200)
+    print(f"Best baseline: {best_baseline}")
+    print("*" * 200)
 
     # Compute compression results
     for compression_ratio in COMPRESSION_RATIOS:
         hyperparameters = best_baseline
-        hyperparameters['compression_ratio'] = compression_ratio
-        hyperparameters['hash_function'] = hash_function
+        hyperparameters["compression_ratio"] = compression_ratio
+        hyperparameters["hash_function"] = hash_function
 
         if hyperparameters in results:
-            print('Compression computed')
+            print("Compression computed")
             continue
 
         print(hyperparameters)
         model = build_factorization_model(hyperparameters, train, random_state)
-        test_mrr, val_mrr, elapsed = evaluate_model(model, train, test, validation)
+        test_mrr, val_mrr, test_rmse, val_rmse, elapsed = evaluate_model(
+            model, train, test, validation
+        )
 
-        print(f'Test MRR: {test_mrr.mean()}, Validation MRR: {val_mrr.mean()}, Elapsed Time: {elapsed}')
-        results.save(hyperparameters, test_mrr.mean(), val_mrr.mean(), elapsed)
-        print('*' * 200)
+        print(
+            f"Test MRR: {test_mrr.mean()},\
+                Validation MRR: {val_mrr.mean()},\
+                Test RMSE: {test_rmse}, \
+                Validation RMSE: {val_rmse},\
+                Elapsed Time: {elapsed}"
+        )
+        results.save(
+            hyperparameters,
+            test_mrr.mean(),
+            val_mrr.mean(),
+            test_rmse,
+            val_rmse,
+            elapsed,
+        )
+        print("*" * 200)
 
     return results
 
 
-def run_experiment(variant='100K', save_path='../results'):
+def run_experiment(variant="100K", save_path="../results"):
     random_state = np.random.RandomState(100)
     dataset = get_movielens_dataset(variant)
     test_percentage = 0.2
 
-    train, rest = random_train_test_split(dataset, test_percentage=test_percentage, random_state=random_state)
-    test, validation = random_train_test_split(rest, test_percentage=0.5, random_state=random_state)
+    train, rest = random_train_test_split(
+        dataset, test_percentage=test_percentage, random_state=random_state
+    )
+    test, validation = random_train_test_split(
+        rest, test_percentage=0.5, random_state=random_state
+    )
 
     for hash_function in HASH_FUNCTIONS:
-        filename = f'{hash_function}_implicit_movielens_results.txt'
+        filename = f"{hash_function}_implicit_movielens_results.txt"
         experiment_name = os.path.join(save_path, filename)
         run(experiment_name, hash_function, train, test, validation, random_state)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_experiment()
--- experiments/results.py
+++ experiments/results.py
@@ -4,39 +4,55 @@ import json
 
 
 class Results:
-
     def __init__(self, filename):
         self._filename = filename
-        open(self._filename, 'a+')
+        if not os.path.exists(filename):
+            os.makedirs(os.path.dirname(filename))
+        open(self._filename, "a+")
 
-    def save(self, hyperparams, test_mrr, validation_mrr, elapsed):
+    def save(
+        self, hyperparams, test_mrr, validation_mrr, test_rmse, validation_rmse, elapsed
+    ):
         result = hyperparams.copy()
-        result.update({'test_mrr': test_mrr, 'validation_mrr': validation_mrr, 'elapsed': elapsed, 'hash': self._hash(hyperparams)})
+        result.update(
+            {
+                "test_mrr": test_mrr,
+                "validation_mrr": validation_mrr,
+                "test_rmse": test_rmse.item(),
+                "validation_rmse": validation_rmse.item(),
+                "elapsed": elapsed,
+                "hash": self._hash(hyperparams),
+            }
+        )
 
-        with open(self._filename, 'a+') as out:
-            out.write(json.dumps(result) + '\n')
+        with open(self._filename, "a+") as out:
+            out.write(json.dumps(result) + "\n")
 
     def best_baseline(self):
-        baseline_results = [x for x in self if x['compression_ratio'] == 1.0 and x['embedding_dim'] >= 32]
-        results = sorted(baseline_results, key=lambda x: -x['test_mrr'])
+        baseline_results = [
+            x
+            for x in self
+            if x["compression_ratio"] == 1.0 and x["embedding_dim"] >= 32
+        ]
+        results = sorted(baseline_results, key=lambda x: -x["test_mrr"])
         return results[0] if results else None
 
     def best(self):
-        results = sorted([x for x in self], key=lambda x: -x['test_mrr'])
+        results = sorted([x for x in self], key=lambda x: -x["test_mrr"])
         return results[0] if results else None
 
     def get_filename(self):
-        return os.path.basename(self._filename).split('_')[0]
+        return os.path.basename(self._filename).split("_")[0]
 
     def __getitem__(self, hyperparams):
         params_hash = self._hash(hyperparams)
 
-        with open(self._filename, 'r+') as fle:
+        with open(self._filename, "r+") as fle:
             for line in fle:
                 datum = json.loads(line)
 
-                if datum['hash'] == params_hash:
-                    del datum['hash']
+                if datum["hash"] == params_hash:
+                    del datum["hash"]
                     return datum
 
         raise KeyError
@@ -49,12 +65,13 @@ class Results:
             return False
 
     def __iter__(self):
-        with open(self._filename, 'r+') as fle:
+        with open(self._filename, "r+") as fle:
             for line in fle:
                 datum = json.loads(line)
-                del datum['hash']
+                del datum["hash"]
                 yield datum
 
     @staticmethod
     def _hash(x):
-        return hashlib.md5(json.dumps(x, sort_keys=True).encode('utf-8')).hexdigest()
+        return hashlib.md5(json.dumps(x, sort_keys=True).encode("utf-8")).hexdigest()
+
